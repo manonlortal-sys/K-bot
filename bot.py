4dd88dd7e851fbefe566492b8cb2a717147c9eea
manonlortal-sys <manonlortal@gmail.com>
2025-09-25 19:31:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,22 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 # === Intents / Bot ===
 intents = discord.Intents.default()
-intents.members = True  # important pour on_member_join
-intents.message_content = True  # utile si tu gardes des commandes prefix "!"
+intents.members = True              # nécessaire pour on_member_join
+intents.message_content = True      # utile si tu gardes des commandes préfixées
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 # === Chargement des cogs et sync des commandes slash ===
 async def setup_hook():
     await bot.load_extension("cogs.welcome")
     await bot.load_extension("cogs.tickets")
+    await bot.load_extension("cogs.stocks")
 
     # Synchronisation globale des slash commands
     await bot.tree.sync()
     print("Slash commands synchronisées.")
 
-
 bot.setup_hook = setup_hook
-
 
 # === Lancement du bot ===
 if __name__ == "__main__":
