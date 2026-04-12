e852c492f2406bf9002a0861f665e7809855a34d
manonlortal-sys <manonlortal@gmail.com>
2026-04-12 11:03:03 +0200

    Update team_service.py

--- services/team_service.py
+++ services/team_service.py
@@ -1,1 +1,15 @@
+from core.state import tournament
 
+def create_team(captain_id: int, players: list):
+    players = list(set(players + [captain_id]))
+
+    team = {
+        "id": str(captain_id),
+        "name": None,
+        "captain_id": captain_id,
+        "players": players
+    }
+
+    tournament["teams"].append(team)
+
+    return team
