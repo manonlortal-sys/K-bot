033d5edac0d6cf5bbb5e0d93df595917e0e45e14
manonlortal-sys <manonlortal@gmail.com>
2026-04-12 11:01:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 import threading
 
-from config import TOKEN
+from config import DISCORD_TOKEN, INSCRIPTION_CHANNEL
 from web.flask_app import app
+from views.inscription_view import InscriptionView
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,20 +13,28 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-@bot.event
-async def on_ready():
-    print(f"✅ Bot connecté en tant que {bot.user}")
-    print("🎮 Tournoi bot prêt")
-
-
-# 🌐 Flask runner
 def run_flask():
     app.run(host="0.0.0.0", port=10000)
 
 
-if __name__ == "__main__":
-    # Flask en thread séparé
-    threading.Thread(target=run_flask).start()
+@bot.event
+async def on_ready():
+    print("Bot OK")
 
-    # Discord bot principal
-    bot.run(TOKEN)
+    channel = bot.get_channel(INSCRIPTION_CHANNEL)
+
+    if channel:
+        embed = discord.Embed(
+            title="TOURNOI DOFUS TOUCH",
+            description="Clique sur participer",
+            color=0x9b59b6
+        )
+
+        await channel.send(embed=embed, view=InscriptionView())
+
+
+bot.run(DISCORD_TOKEN)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
