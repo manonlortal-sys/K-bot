9bc125b94f72d76f2018b4b53c58f38f7a0c369f
manonlortal-sys <manonlortal@gmail.com>
2026-04-12 11:00:41 +0200

    Update permissions.py

--- core/permissions.py
+++ core/permissions.py
@@ -1,8 +1,4 @@
-import discord
-from config import ORG_ROLE_ID, PARTICIPANT_ROLE_ID
+from config import ORG_ROLE_ID
 
-def is_organizer(member: discord.Member) -> bool:
+def is_organizer(member):
     return any(role.id == ORG_ROLE_ID for role in member.roles)
-
-def is_participant(member: discord.Member) -> bool:
-    return any(role.id == PARTICIPANT_ROLE_ID for role in member.roles)
