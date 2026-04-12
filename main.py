7cf5b531f78d9fd796d08a7c2ccec3f0ec9b8604
manonlortal-sys <manonlortal@gmail.com>
2026-04-12 12:01:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+import threading
 import discord
 from discord.ext import commands
-import threading
 
 from config import DISCORD_TOKEN, INSCRIPTION_CHANNEL
 from web.flask_app import app
 from views.inscription_view import InscriptionView
-
-# 🔥 Désactive voice (évite audioop)
-discord.VoiceClient = None
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,8 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 def run_flask():
-    port = 10000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
 
 
 @bot.event
@@ -30,15 +26,13 @@ async def on_ready():
     if channel:
         embed = discord.Embed(
             title="TOURNOI DOFUS TOUCH",
-            description="Clique sur 🎮 Je participe",
+            description="Clique sur Je participe",
             color=0x9b59b6
         )
 
         await channel.send(embed=embed, view=InscriptionView())
 
 
-bot.run(DISCORD_TOKEN)
-
-
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
+    bot.run(DISCORD_TOKEN)
